106c910b1030814b48b1d854f5a8ec4cf26f203a
Ruslan Azizov <Maks@maks-macbook-air.home>
2026-06-17 23:12:07 +0200

    to do paginetions

--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -1,5 +1,26 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
 import { PageTitle } from "../../common/components/PageTitle/PageTitle"
+import s from "./CharacterPage.module.css"
+
 
 export const CharacterPage = () => {
-    return <PageTitle style={{fontSize: "70px"}} title="CharacterPage" />
+    const [character, seCharacter] = useState()
+
+    useEffect(() => {
+        axios.get("https://rickandmortyapi.com/api/character?page=2").then((res) => {
+        seCharacter(res.data.results)
+        })
+    }, [])
+ 
+    return <div className="pageContainer"> 
+        <PageTitle style={{fontSize: "70px"}} title="CharacterPage" />
+        <ul className={s.characters}>{character?.map(ch => <li key={ch.id}>
+                <div className={s.character}>
+                <div className={s.characterLink}>{ch.name}</div>
+                <img src={ch.image} alt={`${ch.name} avatar`} />
+            </div>
+        </li>)}</ul>
+        
+    </div>
 }
