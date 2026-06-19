885c1563c1b2d3b92bccfe0529473249caeb4ec2
Ruslan Azizov <Maks@maks-macbook-air.home>
2026-06-19 15:43:10 +0200

    to do pagenetion and refactor

--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -3,24 +3,60 @@ import axios from "axios"
 import { PageTitle } from "../../common/components/PageTitle/PageTitle"
 import s from "./CharacterPage.module.css"
 
-
+ 
 export const CharacterPage = () => {
     const [character, seCharacter] = useState()
+     const [info, setInfo] = useState({
+        count: 0,
+        pages: 0,
+        next: null,
+        prev: null,
+      }
+    )
+
+    const fetchData = (url) => {
+    axios.get(url).then((res) => {
+      seCharacter(res.data.results)
+      setInfo(res.data.info)
+    })
+  }
+
 
     useEffect(() => {
         axios.get("https://rickandmortyapi.com/api/character?page=2").then((res) => {
         seCharacter(res.data.results)
+        setInfo(res.data.info)
         })
     }, [])
  
-    return <div className="pageContainer"> 
+    const nextPageHandler = () => {
+       fetchData(info.next)
+    }
+ 
+    const previousPageHandler = () => {
+       fetchData(info.prev)
+    }
+
+return( 
+
+    <div className="pageContainer"> 
         <PageTitle style={{fontSize: "70px"}} title="CharacterPage" />
-        <ul className={s.characters}>{character?.map(ch => <li key={ch.id}>
+        <ul className={s.characters}>{character?.map(ch => (
+            <li key={ch.id}>
                 <div className={s.character}>
-                <div className={s.characterLink}>{ch.name}</div>
-                <img src={ch.image} alt={`${ch.name} avatar`} />
-            </div>
-        </li>)}</ul>
-        
+                    <div className={s.characterLink}>{ch.name}</div>
+                    <img src={ch.image} alt={`${ch.name} avatar`} />
+                </div>
+            </li>
+        ))}    
+        </ul>
+        <div className={s.buttonContainer}>
+            <button className={s.linkButton} disabled={info.prev === null} onClick={previousPageHandler}>
+              Назад
+            </button>
+            <button className={s.linkButton} disabled={info.next === null} onClick={nextPageHandler}>
+              Вперед
+            </button>
+        </div>
     </div>
-}
+)}
