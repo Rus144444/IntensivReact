1657fdcc06ac4cba9c144f5684340a03f6f0bed4
Ruslan Azizov <Maks@maks-macbook-air.home>
2026-06-19 17:00:26 +0200

    rename derectore CharectersPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route} from "react-router"
 import {Header} from "./common/components/Header/Header"
 import {HomePage} from "./pages/HomePage/HomePage"
-import {CharacterPage} from "./pages/CharacterPage/CharacterPage"
+import {CharacterPage} from "./pages/CharactersPage/CharactersPage"
 import {EpisodePage} from "./pages/EpisodePage/EpisodePage"
 import {LocationsPage} from "./pages/LocationsPage/LocationsPage"
 import './App.css'
--- src/pages/CharactersPage/CharactersPage.jsx
+++ src/pages/CharactersPage/CharactersPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 import { PageTitle } from "../../common/components/PageTitle/PageTitle"
-import s from "./CharacterPage.module.css"
+import s from "./CharactersPage.module.css"
 
  
 export const CharacterPage = () => {
