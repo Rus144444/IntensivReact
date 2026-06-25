2cdb7b8e6a8e9bf80341d20a9e14bc3d90cb9f03
Ruslan Azizov <Maks@maks-macbook-air.home>
2026-06-25 20:20:55 +0200

    seporate logic in LocatiosPage.jsx to LocationsPage.js

--- src/UI/pages/LocationsPage/LocationsPage.jsx
+++ src/UI/pages/LocationsPage/LocationsPage.jsx
@@ -1,50 +1,9 @@
-import { useEffect, useState } from "react"
 import {PageTitle} from "../../common/components/PageTitle/PageTitle"
-import axios from "axios"
 import { Link } from "react-router"
+import { useLocationsPage } from "../../../BLL/useLocationsPage"
 
 export const LocationsPage = () => {
-   const [locations, setLocations] = useState()
-   const [error, setError] = useState(null)
-   const [info, setInfo] = useState({
-        count: 0,
-        pages: 0,
-        next: null,
-        prev: null,
-      }
-   )
-
-   useEffect(() => {
-      axios.get("https://rickandmortyapi.com/api/location")
-      .then((res)=> {
-         setLocations(res.data.results)
-         setInfo(res.data.info)
-      })
-   
-   }, [])
-
-   const fetchData = (url) => {
-    axios.get(url).then((res) => {
-      setLocations(res.data.results)
-      setInfo(res.data.info)
-      setError(null)
-    })
-    .catch((err) => {
-        setError(err.response.data.error)
-      })
-  }
-
-  const searchHandler = (event) => {
-    const value = event.currentTarget.value
-    fetchData(`https://rickandmortyapi.com/api/location?name=${value}`)
-  }
- 
-    const nextPageHandler = () => {
-       fetchData(info.next)
-    }
-    const previousPageHandler = () => {
-       fetchData(info.prev)
-    }
+   const {info,error,locations,previousPageHandler,nextPageHandler,searchHandler} = useLocationsPage()
    return (
       <div>
          <PageTitle title="Location" style={{fontSize: "70px"}}/>
