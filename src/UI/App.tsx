01f05aada315d90f3ed5cdac019c00338f7fe30f
Ruslan Azizov <Maks@maks-macbook-air.home>
2026-06-29 04:46:28 +0200

    fix and raname fails

--- src/UI/App.tsx
+++ src/UI/App.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route} from "react-router"
-import { Header } from "./common/components/Header/Header"
-import { HomePage } from "./pages/HomePage/HomePage"
-import { CharactersPage } from "./pages/CharactersPage/CharactersPage"
-import { Character} from "./pages/CharactersPage/Charecter/Charecter"
-import { EpisodePage} from "./pages/EpisodePage/EpisodePage"
-import { Episode } from "./pages/EpisodePage/Episode/Episode"
-import { LocationsPage } from "./pages/LocationsPage/LocationsPage"
+import { Header } from "./common/components/Header/Header.tsx"
+import { HomePage } from "./pages/HomePage/HomePage.tsx"
+import { CharactersPage } from "./pages/CharactersPage/CharactersPage.tsx"
+import { Character} from "./pages/CharactersPage/Charecter/Charecter.tsx"
+import { EpisodePage} from "./pages/EpisodePage/EpisodePage.tsx"
+import { Episode } from "./pages/EpisodePage/Episode/Episode.tsx"
+import { LocationsPage } from "./pages/LocationsPage/LocationsPage.tsx"
 import { Location } from "./pages/LocationsPage/Location/Location.tsx"
-import { PATH } from "./common/routing/path"
+import { PATH } from "./common/routing/path.ts"
 
 function App() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import "./index.css"
 
 const root = document.getElementById("root");
 
+if (!root) {
+  throw new Error("Root element not found");
+}
+
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <App/>
